bdbeb2aeacb02c7b4846a01a5ada26c19e05a408
Bolin Zheng <82277273+zhengbz0111@users.noreply.github.com>
2022-07-25 19:16:33 +1000

    Add change shape C# file

---
+++ Assets/Scripts/ChangeShapeto.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeShapeto : MonoBehaviour
+{
+    [SerializeField] private Mesh ChangeShapeTo;
+
+        private void OnCollisionEnter()
+    {
+        // First we'll get the material used by this game object's 'Renderer'.
+        var meshFilter = GetComponent<MeshFilter>();
+
+        meshFilter.sharedMesh = this.ChangeShapeTo;
+    }
+}
